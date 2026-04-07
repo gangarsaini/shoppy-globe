2be5d4ce5a6f363d27d2b02d4af04911a4f29651
Rani <saini12.rani@gmail.com>
2026-04-07 15:27:53 +0530

    modify the hook

--- src/hooks/useProductDetail.js
+++ src/hooks/useProductDetail.js
@@ -1,22 +1,37 @@
 import { useEffect, useState } from "react";
 
 function useProductDetail(id){
-    const [product , setProduct] = useState(null)
+    const [product , setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(()=>{
         // function to fetch data based on id
+        setLoading(true);
+        setError(null);
         async function fetchProduct() {
+            try{
             const res = await fetch( `https://dummyjson.com/products/${id}`)
-
+            if(!res.ok){
+                throw new Error("Failed to fetch product data");
+            }
             const data = await res.json()
             //  setting up the product fetched to useState variable product 
-            setProduct(data)
+            setProduct(data);
+            
+            }
+            catch(err){
+              setError(err.msg)
+            }
+            finally{
+                setLoading(false)
+            }
         }
         fetchProduct()
 
     },[id])
 
-    return product
+    return {product,loading,error}
 }
 
-export default useProductDetail
+export default useProductDetail;
